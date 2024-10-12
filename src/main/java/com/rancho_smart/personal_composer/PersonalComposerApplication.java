4aad86f415ed9482d987c02057bcb88ae975fe53
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:48:40 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/personal_composer/PersonalComposerApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.personal_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalComposerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/personal_composer/PersonalComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.personal_composer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
